1cab487dca2d56959e3f2c77f5b9ff365ed31fb1
Stacey Lewis <stacey.lws@googlemail.com>
2023-11-24 22:11:15 +0000

    Add: Add event input icons and datepicker

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
         let sidenavs = document.querySelectorAll(".sidenav");
         let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+
+        var elems = document.querySelectorAll('.datepicker');
+        var instances = M.Datepicker.init(elems, {
+            format: "dd mmmm, yyyy",
+            yearRange: 3,
+            showClearBtn: true,
+            i18n: {
+                done: "Select"
+            }
+        });
     });
-  
