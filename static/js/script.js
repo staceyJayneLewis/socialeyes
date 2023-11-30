c2bc5a8523764397c3944bc905906abf1ea6e6ae
Stacey Lewis <stacey.lws@googlemail.com>
2023-11-30 17:24:27 +0000

    feature: implement the date and time format tool

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,34 @@
+/* jshint esversion: 11, jquery: true */
 document.addEventListener('DOMContentLoaded', function () {
-        let sidenavs = document.querySelectorAll(".sidenav");
-        let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-
-        var elems = document.querySelectorAll('.datepicker');
-        var instances = M.Datepicker.init(elems, {
-            format: "dd mmmm, yyyy",
-            yearRange: 3,
-            showClearBtn: true,
-            i18n: {
-                done: "Select"
-            }
-        });
+    // mobile sidebar
+    let sidenavs = document.querySelectorAll(".sidenav");
+    let sidenavsInstance = M.Sidenav.init(sidenavs, {
+        edge: "right"
     });
+
+    // date picker initialization
+    let datePickers = document.querySelectorAll('.datepicker');
+    let datePickerInstances = M.Datepicker.init(datePickers, {
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
+
+     // time picker initialization
+     let timePickers = document.querySelectorAll('.timepicker');
+     let timePickerInstances = M.Timepicker.init(timePickers, {
+       
+         showClearBtn: true,
+         i18n: {
+             done: "Select",
+             cancel: "cancel",
+             clear: "clear",
+         }
+     });
+    let modals = document.querySelectorAll('.modal');
+    let modalInstances = M.Modal.init(modals, {});
+
+});
